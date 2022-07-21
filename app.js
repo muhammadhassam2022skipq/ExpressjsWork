0074eaa42a38abc5f7fac387d84e775d597ad7a7
muhammadhassam2022skipq <muhammad.hassam.skipq@gmail.com>
2022-07-21 22:44:40 +0500

    Night Practice

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
 const express = require("express");
-const bodyParser= require ("body-parser");
-const homeRoute = require ("./routes/home");
-// const aboutRoute = reqiure ("./routes/about");
-const productRoute= require("./routes/Product");
-const errorRoute= require("./routes/notfound");
-const contactRoute= require("./routes/contact");
+const bodyParser = require("body-parser");
+const homeRoute = require("./routes/home");
+const productRoute = require("./routes/Product");
+const errorRoute = require("./routes/notfound");
+const contactRoute = require("./routes/contact");
 //creating express app
-const app= express();
+const app = express();
 // body parsing
 app.use(bodyParser.urlencoded({ extended: false }));
-app.set ("views", "views");
+app.set("views", "views");
 app.set("view engine", "ejs");
 
-
 //using module routes
-app.use (homeRoute);
-// app.use(aboutRoute);
-app.use("/shopping",productRoute);
-app.use (errorRoute);
-app.use (contactRoute);
+app.use(homeRoute);
+app.use("/shopping", productRoute);
+app.use(errorRoute);
+app.use(contactRoute);
 
 // Listening to the port
-app.listen (3000, (req, res)=> {
-    console.log ("Listening on port 3000");
-})
+app.listen(3000, (req, res) => {
+  console.log("http://localhost/" + 3000);
+});
--- routes/contact.js
+++ routes/contact.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
+var x=0;
+var y=0;
+const products= [];
 
 router.get("/contact", (req, res) => {
-    res.render("about");
+    res.render("contact");
 })
+router.post("/contact", (req, res) => {
+    x=parseInt(req.body.pname);
+    y= parseInt(req.body.id);
+    console.log(y);
+    res.render("contact", {
+        products:[
+            {fname : "x"},
+            {ID: y}
+        ]
+    })
+});
 
 module.exports = router;
